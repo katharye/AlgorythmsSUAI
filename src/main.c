f47b6c1f57048b91ad1b9e7ff766dfb5c40ff0a8
katharye <multiden007@gmail.com>
2025-12-09 04:05:42 +0300

    added condition for lab 2

--- src/main.c
+++ src/main.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 
 #include "lab1/lab1.h"
+#include "lab2/lab2.h"
 
 void clear();
 void print_quest(char* filename);
 
 int main() {
+    clear();
     printf("Choose number of lab work:\n1 - lab1\n2 - lab2\n  \n");
     int lab_num;
     if (scanf("%d", &lab_num) != 1) {
@@ -25,11 +27,21 @@ int main() {
         scanf("%s", &filename);
         lab1(filename);
         break;
-    
+    case 2:
+        clear();
+        print_quest("lab2/quest.txt");
+
+        lab2();
+        break;
+
     default:
         printf("Incorrect number!\n");
         return 1;
     }
+    
+    
+    printf("\n\nCheck another lab? (Y, n)");
+    
 }
 
 void clear() {
